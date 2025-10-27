b7a99e0f4003b023b92e652c2b795b37601d3982
Stanley <78755021+netswitchsf@users.noreply.github.com>
2025-10-27 18:49:41 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 80,
+    allowedHosts: ['roi.securli.hk'],
+    hmr: {
+      overlay: false
+    }
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  optimizeDeps: {
+    entries: ["./src/main.tsx"]
+  }
+}));
